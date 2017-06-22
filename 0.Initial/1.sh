1bbfb1cda13dc34e3f8dad7b2d19b85ed65efe4c
alphaaurigae <88888mb@gmail.com>
2017-06-22 02:45:28 +0200

    fix

--- 0.Initial/1.sh
+++ 0.Initial/1.sh
@@ -5,13 +5,13 @@
 #
 # 
 srcdPT1=/opt/ITSEC-Install-Scripts/0.Initial/src/PT1
-chkdir=/opt/ITSEC-Install-Scripts/0.Initial/src
+#chkdir=/opt/ITSEC-Install-Scripts/0.Initial/src
 
 cd $srcdPT1
 ./1-Upgrade.sh
 
 #Calling the reboot-script to restart this script 
-echo "#helloworld" > $chkdir/rebootchkPT1.txt
+#echo "#helloworld" > $chkdir/rebootchkPT1.txt
 
 #reboot
 
