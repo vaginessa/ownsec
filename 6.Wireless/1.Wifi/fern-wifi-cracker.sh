2ba202632939f8cfb2708161f7515866461bbcca
alphaaurigae <88888mb@gmail.com>
2017-06-26 12:46:44 +0200

    fern-wifi-cracker console fix

--- 6.Wireless/1.Wifi/fern-wifi-cracker.sh
+++ 6.Wireless/1.Wifi/fern-wifi-cracker.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+echo "${bold}
+ _____ _____ ____  _   _ 
+|  ___| ____|  _ \| \ | |
+| |_  |  _| | |_) |  \| |
+|  _| | |___|  _ <| |\  |
+|_|   |_____|_| \_\_| \_|
+          
+${normal}"
+
 mkdir -p /opt/ITSEC/6.Wireless/1.Wifi/fern-wifi-cracker/savio-code
 cd /opt/ITSEC/6.Wireless/1.Wifi/fern-wifi-cracker/savio-code
 git clone https://github.com/savio-code/fern-wifi-cracker.git
@@ -16,18 +28,6 @@ DSKTPFLS=/opt/ITSEC-Install-Scripts/0.Initial/usrlcl/.local/share/applications/6
 DSKTPFLSDEST=/home/$USER/.local/share/applications/6.Wireless/1.Wifi
 DSKTPFL=fern-wifi-cracker.desktop
 
-bold=$(tput bold)
-normal=$(tput sgr0)
-
-echo "${bold}
- _____ _____ ____  _   _ 
-|  ___| ____|  _ \| \ | |
-| |_  |  _| | |_) |  \| |
-|  _| | |___|  _ <| |\  |
-|_|   |_____|_| \_\_| \_|
-          
-${normal}"
-
 
 cd $GITREPOROOT
 sudo rm -f /usr/local/bin/$EXECUTEABLE2
@@ -35,12 +35,13 @@ git clean -f
 git fetch origin
 git reset --hard origin/master
 git pull
-echo "#!/bin/bash
+echo "#!/bin/bash 
+
 cd /opt/ITSEC/6.Wireless/1.Wifi/fern-wifi-cracker/savio-code/fern-wifi-cracker/Fern-Wifi-Cracker
 
 python execute.py" > $EXECUTEABLE2
 sudo chmod +x $EXECUTEABLE1
 sudo chmod +x $EXECUTEABLE2
-sudo ln -s $GITREPOROOT/$EXECUTEABLE1 /usr/local/bin/$EXECUTEABLE2
+sudo ln -s $GITREPOROOT/$EXECUTEABLE2 /usr/local/bin/$EXECUTEABLE2
 mkdir -p $DSKTPFLSDEST
 cp $DSKTPFLS/$DSKTPFL $DSKTPFLSDEST/$DSKTPFL
