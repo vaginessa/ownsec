48ec6dacc3470e9dd651fc78a24763781383bd13
tux <tux@1>
2017-06-27 21:57:34 +0200

    bettercap rbenv ...

--- 7.Mitm/bettercap.sh
+++ 7.Mitm/bettercap.sh
@@ -48,33 +48,73 @@ git pull
 git submodule init
 git submodule update --recursive
 echo "${bold}
-test000            
+gem install bundler            
 ${normal}"
 gem install bundler
 echo "${bold}
-test00            
+bundle update         
 ${normal}"
 bundle update
 echo "${bold}
-test0            
+bundle install         
 ${normal}"
 bundle install
 echo "${bold}
-test            
+gem update          
 ${normal}"
 gem update
 
+echo "${bold}
+gem build bettercap.gemspec          
+${normal}"
 gem build bettercap.gemspec
 echo "${bold}
-test2            
+cd to bin dir          
 ${normal}"
+cd /opt/ITSEC/7.Mitm/bettercap/evilsocket/bettercap/bin/
 
-
-gem install bettercap*.gem
-echo "${bold}
-test3            
-${normal}"
+#gem install bettercap*.gem
+#echo "${bold}
+#test3            
+#${normal}"
 #rbenv sudo bettercap
 
+echo "${bold}
+write sh file to run with the desired rbenv ruby            
+${normal}"
+echo '#!/bin/bash -i
+
+cd /opt/ITSEC/7.Mitm/bettercap/evilsocket/bettercap/bin/
+
+source ~/.bashrc
+eval "$(rbenv init -)"
+rbenv shell 2.4.1
+./bettercap
+' > bettercap.sh
+
+echo "${bold}
+chmod +x the sh file        
+${normal}"
+chmod +x bettercap.sh
+
+echo "${bold}
+rm the old usr bin link       
+${normal}"
+sudo rm -f /usr/local/bin/bettercap
+echo "${bold}
+symlink the sh file      
+${normal}"
+sudo ln -s /opt/ITSEC/7.Mitm/bettercap/evilsocket/bettercap/bin/bettercap.sh /usr/local/bin/bettercap
+
+echo "${bold}
+make desktop file dir      
+${normal}"
 mkdir -p $DSKTPFLSDEST
+echo "${bold}
+cp desktop files for the menu     
+${normal}"
 cp $DSKTPFLS/$DSKTPFL $DSKTPFLSDEST/$DSKTPFL
+
+echo "${bold}
+done running the bettercap setup   
+${normal}"
