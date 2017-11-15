97aec58b5686ee01ec985b8ca4b845f95b345790
alphaaurigae <robotux@1>
2017-11-15 06:07:28 +0100

    f-scrack fix, @ysrc deleted repo, switch to @y1ng1996

--- 4.Password/1.Network/f-scrack/f-scrack.sh
+++ 4.Password/1.Network/f-scrack/f-scrack.sh
@@ -12,11 +12,18 @@ echo "${bold}
              
 ${normal}"
 
-mkdir -p /opt/ITSEC/4.Password/1.Network/f-scrack/ysrc
-cd /opt/ITSEC/4.Password/1.Network/f-scrack/ysrc
-git clone https://github.com/ysrc/F-Scrack.git
+#mkdir -p /opt/ITSEC/4.Password/1.Network/f-scrack/ysrc
+#cd /opt/ITSEC/4.Password/1.Network/f-scrack/ysrc
+#git clone https://github.com/ysrc/F-Scrack.git
 
-GITREPOROOT=/opt/ITSEC/4.Password/1.Network/f-scrack/ysrc/F-Scrack
+#GITREPOROOT=/opt/ITSEC/4.Password/1.Network/f-scrack/ysrc/F-Scrack
+
+mkdir -p /opt/ITSEC/4.Password/1.Network/f-scrack/y1ng1996
+cd /opt/ITSEC/4.Password/1.Network/f-scrack/y1ng1996
+git clone https://github.com/y1ng1996/F-Scrack.git
+
+GITREPOROOT=/opt/ITSEC/4.Password/1.Network/f-scrack/y1ng1996/F-Scrack
+
 EXECUTEABLE1=F-Scrack.py
 EXECUTEABLE2=fscrack
 EXECUTEABLE3=fscrack.sh
@@ -39,7 +46,8 @@ git submodule update --recursive
 
 echo '#!/bin/bash
 
-cd /opt/ITSEC/4.Password/1.Network/f-scrack/ysrc/F-Scrack
+#cd /opt/ITSEC/4.Password/1.Network/f-scrack/ysrc/F-Scrack
+cd /opt/ITSEC/4.Password/1.Network/f-scrack/y1ng1996/F-Scrack
 
 python F-Scrack.py "$@"' > $EXECUTEABLE3
 chmod +x $GITREPOROOT/$EXECUTEABLE1
