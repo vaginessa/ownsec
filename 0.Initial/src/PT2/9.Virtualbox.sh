b5ebd8347078144380225f759eb4f0cc958afbfc
alphaaurigae <robotux@1>
2017-11-26 21:30:54 +0100

    virtualbox update

--- 0.Initial/src/PT2/9.Virtualbox.sh
+++ 0.Initial/src/PT2/9.Virtualbox.sh
@@ -22,8 +22,8 @@ cd /opt/DEPS
 sudo rm -r virtualbox
 mkdir virtualbox
 cd virtualbox
-wget http://download.virtualbox.org/virtualbox/5.2.0/virtualbox-5.2_5.2.0-118431~Ubuntu~xenial_amd64.deb
-yes | sudo dpkg -i virtualbox-5.2_5.2.0-118431~Ubuntu~xenial_amd64.deb
+wget http://download.virtualbox.org/virtualbox/5.2.2/virtualbox-5.2_5.2.2-119230~Ubuntu~xenial_amd64.deb
+yes | sudo dpkg -i virtualbox-5.2_5.2.2-119230~Ubuntu~xenial_amd64.deb
 
-#wget http://download.virtualbox.org/virtualbox/5.2.0/Oracle_VM_VirtualBox_Extension_Pack-5.2.0-118431.vbox-extpack
-#yes | sudo VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-5.2.0-118431.vbox-extpack
+#wget http://download.virtualbox.org/virtualbox/5.2.2/Oracle_VM_VirtualBox_Extension_Pack-5.2.2-119230.vbox-extpack
+#yes | sudo VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-5.2.2-119230.vbox-extpack
