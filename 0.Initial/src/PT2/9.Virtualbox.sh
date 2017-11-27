2b500966d0dbe8d5a7b1e4f8946ceb698c7e4500
alphaaurigae <robotux@1>
2017-11-27 22:20:58 +0100

    virtualbox deb add & evil-droid add

--- 0.Initial/src/PT2/9.Virtualbox.sh
+++ 0.Initial/src/PT2/9.Virtualbox.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-
-sudo rm -rf /opt/DEPS/virtualbox
-mkdir -p /opt/DEPS/virtualbox
-
-
 bold=$(tput bold)
 normal=$(tput sgr0)
 
@@ -17,13 +12,7 @@ __     _____ ____ _____ _   _   _    _     ____   _____  __
   
 ${normal}"
 
-
-cd /opt/DEPS
-sudo rm -r virtualbox
-mkdir virtualbox
-cd virtualbox
-wget http://download.virtualbox.org/virtualbox/5.2.2/virtualbox-5.2_5.2.2-119230~Ubuntu~xenial_amd64.deb
-yes | sudo dpkg -i virtualbox-5.2_5.2.2-119230~Ubuntu~xenial_amd64.deb
-
-#wget http://download.virtualbox.org/virtualbox/5.2.2/Oracle_VM_VirtualBox_Extension_Pack-5.2.2-119230.vbox-extpack
-#yes | sudo VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-5.2.2-119230.vbox-extpack
+sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" >> /etc/apt/sources.list.d/virtualbox.list' 
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install VirtualBox-5.2
---
+++ 9.Maintain-Access/1.OS-Backdoors/4.Android/evil-droid.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+GITREPO=https://github.com/M4sc3r4n0/Evil-Droid
+BRANCH=master
+GITREPOROOT=/opt/ITSEC/9.Maintain-Access/1.OS-Backdoors/4.Android-Backdoor/evil-droid/M4sc3r4n0/Evil-Droid
+GITCLONEDIR=/opt/ITSEC/9.Maintain-Access/1.OS-Backdoors/4.Android-Backdoor/evil-droid/M4sc3r4n0
+EXECUTEABLE1=evil-droid
+EXECUTEABLE2=evil-droid
+BINDIR=/usr/local/bin
+DSKTPFLS=/opt/ITSEC-Install-Scripts/0.Initial/usrlcl/.local/share/applications/9.Maintain-Access/1.OS-Backdoors/4.Android-Backdoor
+DSKTPFLSDEST=/home/$USER/.local/share/applications/9.Maintain-Access/1.OS-Backdoors/4.Android-Backdoor
+DSKTPFL=evil-droid.desktop
+GITSBMDLINIT () {
+	git submodule init
+	git submodule update --recursive
+	sudo updatedb && sudo ldconfig
+}
+echo "${bold}
+ _______     _____ _          ____  ____   ___ ___ ____  
+| ____\ \   / /_ _| |        |  _ \|  _ \ / _ \_ _|  _ \ 
+|  _|  \ \ / / | || |   _____| | | | |_) | | | | || | | |
+| |___  \ V /  | || |__|_____| |_| |  _ <| |_| | || |_| |
+|_____|  \_/  |___|_____|    |____/|_| \_\\___/___|____/ 
+                                                         
+INSTALL   
+${normal}"
+
+mkdir -p $GITCLONEDIR
+cd $GITCLONEDIR
+git clone -b $BRANCH $GITREPO
+cd $GITREPOROOT
+
+GITSBMDLINIT
+
+chmod +x $EXECUTEABLE1
+
+sudo rm -f $BINDIR/$EXECUTEABLE2
+sudo ln -s $GITREPOROOT/$EXECUTEABLE1 $BINDIR/$EXECUTEABLE2
+mkdir -p $DSKTPFLSDEST
+cp $DSKTPFLS/$DSKTPFL $DSKTPFLSDEST/$DSKTPFL
