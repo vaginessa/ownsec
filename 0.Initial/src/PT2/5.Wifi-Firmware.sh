c7768d2f597343e01d242ae9bc703f148b189876
alphaaurigae <robotux@1>
2017-12-01 17:47:36 +0100

    wifi - Mange/rtl8192eu-linux-driver && aircrack-ng/rtl8812au

--- 0.Initial/src/PT2/5.Wifi-Firmware.sh
+++ 0.Initial/src/PT2/5.Wifi-Firmware.sh
@@ -1,125 +1,51 @@
 #!/bin/bash
 
-
-rtl8812AU
-mkdir -p /opt/DRVR/WIFI/8812/rtl8812au/astsam
-
-cd /opt/DRVR/WIFI/8812/rtl8812au/astsam
-git clone https://github.com/astsam/rtl8812au
-cd rtl8812au
-
-make clean
-make -j 4
-sudo cp 8812au.ko /lib/modules/$(uname -r)/kernel/drivers/net/wireless
-sudo make install
-sudo modprobe -a 8812au
-
-######################################################################
-#rtl8812AU
-#mkdir -p /opt/DRVR/WIFI/8812/rtl8812au/kimocoder
-#cd /opt/DRVR/WIFI/8812/rtl8812au/kimocoder
-#git clone https://github.com/kimocoder/rtl8812au
-#cd rtl8812au
-#make clean
-#git clean -f
-#git pull
-#make -j 4
-#sudo cp 8812au.ko /lib/modules/$(uname -r)/kernel/drivers/net/wireless
-#sudo make install
-#sudo modprobe -a 8812au
-######################################################################
-#mkdir -p /opt/DRVR/WIFI/8812/rtl8812AU/diederikdehaas
-#cd /opt/DRVR/WIFI/8812/rtl8812AU/diederikdehaas
-#git clone https://github.com/diederikdehaas/rtl8812AU
-#cd rtl8812AU
-#make clean
-#git clean -f
-#git pull
-#make -j 4
-#sudo cp 8812au.ko /lib/modules/$(uname -r)/kernel/drivers/net/wireless
-#sudo make install
-#sudo modprobe -a 8812au
-######################################################################
-#mkdir -p /opt/DRVR/WIFI/8812/rtl8812au/gordboy
-#cd /opt/DRVR/WIFI/8812/rtl8812au/gordboy
-#git clone https://github.com/gordboy/rtl8812au
-#cd rtl8812au
-#make clean
-#git clean -f
-#git pull
-#make -j 4
-#sudo cp 8812au.ko /lib/modules/$(uname -r)/kernel/drivers/net/wireless
-#sudo make install
-#sudo modprobe -a 8812au
-######################################################################
-#mkdir -p /opt/DRVR/WIFI/8812/aircrack-ng
-
-#cd /opt/DRVR/WIFI/8812/aircrack-ng
-#git clone -b v5.1.5  https://github.com/aircrack-ng/rtl8812au
-#cd rtl8812au
- 
-#git checkout v5.1.5
-#make clean
-#git clean -f
-#git pull
-#make -j 4
-
-
-########################################################
-
-#evilphish
-
-#rtl8812AU
-#mkdir -p /opt/DRVR/WIFI/8812/evilphish
-
-#cd /opt/DRVR/WIFI/8812/evilphish
-#git clone -b v5.1.5  https://github.com/evilphish/rtl8812au
-#cd rtl8812au
- 
-#git checkout v5.1.5
-#make clean
-#git clean -f
-#git pull
-#make -j 4
-
-############################################
-#rtl8812AU
-mkdir -p /opt/DRVR/WIFI/8812/rtl8812au/gnab
-cd /opt/DRVR/WIFI/8812/rtl8812au/gnab
-git clone https://github.com/gnab/rtl8812au
-cd rtl8812au
-
-make
-sudo insmod 8812au.ko
-sudo cp 8812au.ko /lib/modules/$(uname -r)/kernel/drivers/net/wireless
-sudo depmod
-#############################################
-#RTL8192EU
-#mkdir -p /opt/DRVR/WIFI/8192/rtl8192eu/$BRANCHzorag
-
-#cd /opt/DRVR/WIFI/8192/rtl8192eu/$BRANCHzorag
-#git clone https://github.com/$BRANCHzorag/RTL8192EU-linux
-#cd RTL8192EU-linux
-
-#make clean
-#git clean -f
-#git pull
-#git checkout linux-4.11
-#make -j 4
-#sudo make install
-############################################
-
 #sudo apt-get update
 #sudo apt-get install git linux-headers-generic build-essential dkms
-#mkdir -p /opt/DRVR/WIFI/8192/rtl8192cu-fixes/pvaret
-#cd /opt/DRVR/WIFI/8192/rtl8192cu-fixes/pvaret
-#git clone https://github.com/pvaret/rtl8192cu-fixes
-#cd rtl8192cu-fixes
-#sudo dkms add ./rtl8192cu-fixes
-#sudo dkms install 8192cu/1.10
-#sudo depmod -a
-#sudo cp ./rtl8192cu-fixes/blacklist-native-rtl8192.conf /etc/modprobe.d/
-#make -j 4
-#sudo make install
 
-############################################
+# doesent build on gcc 4.7
+
+#############################################
+####################### 8812
+
+# rtl8812AU
+
+### aircrack-ng/rtl8812au
+### RTL8812AU/21AU and RTL8814AU driver with monitor mode and frame injection 
+
+mkdir -p /opt/DRVR/WIFI/8812/aircrack-ng
+cd /opt/DRVR/WIFI/8812/aircrack-ng
+git clone -b v5.1.5 https://github.com/aircrack-ng/rtl8812au
+cd rtl8812au
+ 
+git checkout v5.1.5
+make clean
+git clean -f
+git pull
+# make -j 4
+# sudo make install
+# sudo cp 8812au.ko /lib/modules/$(uname -r)/kernel/drivers/net/wireless
+chmod +x dkms-install.sh 
+sudo ./dkms-install.sh
+
+#############################################
+####################### 8192
+
+# RTL8192EU
+
+### Mange/rtl8192eu-linux-driver
+### Drivers for the rtl8192eu chipset for wireless adapters (D-Link DWA-131 rev E1 included!) 
+
+mkdir -p /opt/DRVR/WIFI/8192/rtl8192eu-linux-driver/Mange
+cd /opt/DRVR/WIFI/8192/rtl8192eu-linux-driver/Mange
+git clone https://github.com/Mange/rtl8192eu-linux-driver
+cd rtl8192eu-linux-driver
+# cd /usr/src/rtl8192eu
+# sudo make clean
+# sudo make
+# sudo make install
+# sudo modprobe -a rtl8192eu
+sudo dkms add .
+sudo dkms install rtl8192eu/1.0
+
+#############################################
