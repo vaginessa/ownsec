0dc5060e4632f22cc9b20cfd0e160ee0406e49ca
alphaaurigae <robotux@1>
2017-12-26 04:23:42 +0100

    some menu / symlink minor fix

--- 0.Initial/src/PT2/4.UsrEnv_Setup/1.Symlinks_Setup.sh
+++ 0.Initial/src/PT2/4.UsrEnv_Setup/1.Symlinks_Setup.sh
@@ -4,7 +4,7 @@ bold=$(tput bold)
 normal=$(tput sgr0)
 
 REPOROOT=/opt/ITSEC-Install-Scripts
-SETTINGSCRIPTS=/0.Initial/settings-scripts
+SETTINGSCRIPTS=0.Initial/settings-scripts
 BINDIR=/usr/local/bin
 XFCEMNUFLS=.local/share/applications
 
@@ -18,65 +18,169 @@ echo "${bold}
 ... Symlink Services ....         
 ${normal}"
 
-sudo rm -f $BINDIR/LAMP_*
-sudo rm -f $BINDIR/LAPP_*
-sudo rm -f $BINDIR/apache2_*
-sudo rm -f $BINDIR/lighttpd_*
-sudo rm -f $BINDIR/nginx_*
-sudo rm -f $BINDIR/mysql_*
-sudo rm -f $BINDIR/postgresql_*
-sudo rm -f $BINDIR/redis-server_*
-sudo rm -f $BINDIR/php7.0-fpm_*
-sudo rm -f $BINDIR/php7.0-fpm_*
-sudo rm -f $BINDIR/php7.0-fpm_*
+sudo updatedb
 
-cd $REPOROOT/$SETTINGSCRIPTS/startstop/0.Services/0.LAMP
-sudo bash -c 'for SHfiles in $(ls *.sh); do ln -s $REPOROOT/$SETTINGSCRIPTS/startstop/0.Services/0.LAMP/$SHfiles $BINDIR/$SHfiles;done'
-cd $REPOROOT/$SETTINGSCRIPTS/startstop/0.Services/0.LAPP
-sudo bash -c 'for SHfiles in $(ls *.sh); do ln -s $REPOROOT/$SETTINGSCRIPTS/startstop/0.Services/0.LAPP/$SHfiles $BINDIR/$SHfiles;done'
-cd $REPOROOT/$SETTINGSCRIPTS/startstop/0.Services/1.APACHE2
-sudo bash -c 'for SHfiles in $(ls *.sh); do ln -s $REPOROOT/$SETTINGSCRIPTS/startstop/0.Services/1.APACHE2/$SHfiles $BINDIR/$SHfiles;done'
-cd $REPOROOT/$SETTINGSCRIPTS/startstop/0.Services/1.LIGHTTPD
-sudo bash -c 'for SHfiles in $(ls *.sh); do ln -s $REPOROOT/$SETTINGSCRIPTS/startstop/0.Services/1.LIGHTTPD/$SHfiles $BINDIR/$SHfiles;done'
-cd $REPOROOT/$SETTINGSCRIPTS/startstop/0.Services/1.NGINX
-sudo bash -c 'for SHfiles in $(ls *.sh); do ln -s $REPOROOT/$SETTINGSCRIPTS/startstop/0.Services/1.NGINX/$SHfiles $BINDIR/$SHfiles;done'
-cd $REPOROOT/$SETTINGSCRIPTS/startstop/0.Services/2.MYSQL
-sudo bash -c 'for SHfiles in $(ls *.sh); do ln -s $REPOROOT/$SETTINGSCRIPTS/startstop/0.Services/2.MYSQL/$SHfiles $BINDIR/$SHfiles;done'
-cd $REPOROOT/$SETTINGSCRIPTS/startstop/0.Services/2.PSQL
-sudo bash -c 'for SHfiles in $(ls *.sh); do ln -s $REPOROOT/$SETTINGSCRIPTS/startstop/0.Services/2.PSQL/$SHfiles $BINDIR/$SHfiles;done'
-cd $REPOROOT/$SETTINGSCRIPTS/startstop/0.Services/2.REDIS
-sudo bash -c 'for SHfiles in $(ls *.sh); do ln -s $REPOROOT/$SETTINGSCRIPTS/startstop/0.Services/2.REDIS/$SHfiles $BINDIR/$SHfiles;done'
-cd $REPOROOT/$SETTINGSCRIPTS/startstop/0.Services/3.PHP
-sudo bash -c 'for SHfiles in $(ls *.sh); do ln -s $REPOROOT/$SETTINGSCRIPTS/startstop/0.Services/3.PHP/$SHfiles $BINDIR/$SHfiles;done'
-cd $REPOROOT/$SETTINGSCRIPTS/startstop/0.Services
-sudo bash -c 'for SHfiles in $(ls *.sh); do ln -s $REPOROOT/$SETTINGSCRIPTS/startstop/0.Services/$SHfiles $BINDIR/$SHfiles;done'
-cd $REPOROOT/$SETTINGSCRIPTS/startstop/0.Services/3.DNS
-sudo bash -c 'for SHfiles in $(ls *.sh); do ln -s $REPOROOT/$SETTINGSCRIPTS/startstop/0.Services/3.DNS/$SHfiles $BINDIR/$SHfiles;done'
-cd $REPOROOT/$SETTINGSCRIPTS/startstop/0.Services/4.TOR
-sudo bash -c 'for SHfiles in $(ls *.sh); do ln -s $REPOROOT/$SETTINGSCRIPTS/startstop/0.Services/4.TOR/$SHfiles $BINDIR/$SHfiles;done'
-cd $REPOROOT/$SETTINGSCRIPTS/startstop/0.Services/6.Docker
-sudo bash -c 'for SHfiles in $(ls *.sh); do ln -s $REPOROOT/$SETTINGSCRIPTS/startstop/0.Services/6.Docker/$SHfiles $BINDIR/$SHfiles;done'
+#delete symlinks created by mistake
+sudo rm -f /LAMP_*
+sudo rm -f /LAPP_*
+sudo rm -f /apache2_*
+sudo rm -f /lighttpd_*
+sudo rm -f /nginx_*
+sudo rm -f /mysql_*
+sudo rm -f /postgresql_*
+sudo rm -f /redis-server_*
+sudo rm -f /php7.0-fpm_*
+sudo rm -f /dnsmasq_*
+sudo rm -f /conky_*
 
-# script to copy and reload xfce configs from 
-sudo ln -s $REPOROOT/$SETTINGSCRIPTS/this-repo/SCRIPT_copy-XFCE-conf.sh $BINDIR/SCRIPT_copy-XFCE-conf
+cd /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services
+for SHfiles in $(ls *.sh)
+do 
+sudo rm -f /usr/local/bin/$SHfiles
+sudo ln -s /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/$SHfiles /usr/local/bin/$SHfiles 
+done
+
+cd /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/0.LAMP
+for SHfiles in $(ls *.sh)
+do
+sudo rm -f /usr/local/bin/$SHfiles
+sudo ln -s /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/0.LAMP/$SHfiles /usr/local/bin/$SHfiles 
+done
+
+cd /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/0.LAPP
+for SHfiles in $(ls *.sh)
+do
+sudo rm -f /usr/local/bin/$SHfiles
+sudo ln -s /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/0.LAPP/$SHfiles /usr/local/bin/$SHfiles 
+done
+
+cd /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/1.APACHE2
+for SHfiles in $(ls *.sh)
+do
+sudo rm -f /usr/local/bin/$SHfiles
+sudo ln -s /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/1.APACHE2/$SHfiles /usr/local/bin/$SHfiles
+done
+
+echo "test"
+
+cd /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/1.LIGHTTPD
+for SHfiles in $(ls *.sh)
+do
+sudo rm -f /usr/local/bin/$SHfiles
+sudo ln -s /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/1.LIGHTTPD/$SHfiles /usr/local/bin/$SHfiles
+done
+
+cd /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/1.NGINX
+for SHfiles in $(ls *.sh)
+do
+sudo rm -f /usr/local/bin/$SHfiles
+sudo ln -s /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/1.NGINX/$SHfiles /usr/local/bin/$SHfiles
+done
+
+cd /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/2.MYSQL
+for SHfiles in $(ls *.sh)
+do
+sudo rm -f /usr/local/bin/$SHfiles
+sudo ln -s /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/2.MYSQL/$SHfiles /usr/local/bin/$SHfiles
+done
+
+cd /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/2.PSQL
+for SHfiles in $(ls *.sh)
+do
+sudo rm -f /usr/local/bin/$SHfiles
+sudo ln -s /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/2.PSQL/$SHfiles /usr/local/bin/$SHfiles
+done
+
+echo "test2"
+cd /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/2.REDIS
+for SHfiles in $(ls *.sh)
+do
+sudo rm -f /usr/local/bin/$SHfiles
+sudo ln -s /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/2.REDIS/$SHfiles /usr/local/bin/$SHfiles
+done
+
+cd /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/3.PHP
+for SHfiles in $(ls *.sh)
+do
+sudo rm -f /usr/local/bin/$SHfiles
+sudo ln -s /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/3.PHP/$SHfiles /usr/local/bin/$SHfiles
+done
+
+cd /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/3.DNS
+for SHfiles in $(ls *.sh)
+do
+sudo rm -f /usr/local/bin/$SHfiles
+sudo ln -s /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/3.DNS/$SHfiles /usr/local/bin/$SHfiles
+done
+
+cd /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/4.TOR
+for SHfiles in $(ls *.sh)
+do
+sudo rm -f /usr/local/bin/$SHfiles
+sudo ln -s /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/4.TOR/$SHfiles /usr/local/bin/$SHfiles
+done
+
+echo "test3"
+cd /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/5.Conky
+for SHfiles in $(ls *.sh)
+do
+sudo rm -f /usr/local/bin/$SHfiles
+sudo ln -s /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/5.Conky/$SHfiles /usr/local/bin/$SHfiles
+done
+echo "test3s"
+cd /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/6.Docker
+for SHfiles in $(ls *.sh)
+do
+sudo rm -f /usr/local/bin/$SHfiles
+sudo ln -s /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/6.Docker/$SHfiles /usr/local/bin/$SHfiles
+done
+
+echo "test4"
+cd /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/7.Print-Scan
+for SHfiles in $(ls *.sh)
+do
+sudo rm -f /usr/local/bin/$SHfiles
+sudo ln -s /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/7.Print-Scan/$SHfiles /usr/local/bin/$SHfiles
+done
+
+cd /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/8.Bluetooth
+for SHfiles in $(ls *.sh)
+do
+sudo rm -f /usr/local/bin/$SHfiles
+sudo ln -s /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/8.Bluetooth/$SHfiles /usr/local/bin/$SHfiles
+done
+
+cd /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/9.GPS
+for SHfiles in $(ls *.sh)
+do
+sudo rm -f /usr/local/bin/$SHfiles
+sudo ln -s /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/startstop/0.Services/9.GPS/$SHfiles /usr/local/bin/$SHfiles
+done
+
+
+
+# script to copy and reload xfce configs from
+sudo rm -f /usr/local/bin/SCRIPT_copy-XFCE-conf
+sudo ln -s /opt/ITSEC-Install-Scripts/0.Initial/settings-scripts/this-repo/SCRIPT_copy-XFCE-conf.sh /usr/local/bin/SCRIPT_copy-XFCE-conf
 
 # UPDATE script symlinks
-sudo rm -f $BINDIR/updateallthegits.sh
-sudo rm -f $BINDIR/upgrade-os.sh
-cd $REPOROOT/00.Maintain
-sudo ln -s $REPOROOT/00.Maintain/updateallthegits.sh $BINDIR/updateallthegits.sh
-cd $REPOROOT/00.Maintain
-sudo ln -s $REPOROOT/00.Maintain/upgrade-os.sh $BINDIR/upgrade-os.sh
+sudo rm -f /usr/local/bin/updateallthegits.sh
+sudo rm -f /usr/local/bin/upgrade-os.sh
+cd /opt/ITSEC-Install-Scripts/00.Maintain
+sudo ln -s /opt/ITSEC-Install-Scripts/00.Maintain/updateallthegits.sh /usr/local/bin/updateallthegits.sh
+cd /opt/ITSEC-Install-Scripts/00.Maintain
+sudo ln -s /opt/ITSEC-Install-Scripts/00.Maintain/upgrade-os.sh /usr/local/bin/upgrade-os.sh
 
 
 # XFCE menu links
 echo "${bold}
 ... Create XFCE Menu Entries for Services ....         
 ${normal}"
+
 mkdir -p /home/$USER/$XFCEMNUFLS/0.Services
-cp -R $REPOROOT/0.Initial/usrlcl/$XFCEMNUFLS/0.Services /home/$USER/$XFCEMNUFLS
+cp -R /opt/ITSEC-Install-Scripts/0.Initial/usrlcl/$XFCEMNUFLS/0.Services /home/$USER/$XFCEMNUFLS
 mkdir -p /home/$USER/$XFCEMNUFLS/00.Scripts
-cp -R $REPOROOT/0.Initial/usrlcl/$XFCEMNUFLS/00.Scripts /home/$USER/$XFCEMNUFLS
+cp -R /opt/ITSEC-Install-Scripts/0.Initial/usrlcl/$XFCEMNUFLS/00.Scripts /home/$USER/$XFCEMNUFLS
 
-
-
+sudo updatedb
+sudo ldconfig
