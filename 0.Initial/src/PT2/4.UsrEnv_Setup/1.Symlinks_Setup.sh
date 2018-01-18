f41479ad2d648f87a17b89eda938591e11aa0fb6
alphaaurigae <88888mb@gmail.com>
2018-01-18 22:20:24 +0100

    tidy up symlink setup file for part 2

--- 0.Initial/src/PT2/4.UsrEnv_Setup/1.Symlinks_Setup.sh
+++ 0.Initial/src/PT2/4.UsrEnv_Setup/1.Symlinks_Setup.sh
@@ -4,7 +4,7 @@ bold=$(tput bold)
 normal=$(tput sgr0)
 
 REPOROOT=/opt/ownsec
-SETTINGSCRIPTS=0.Initial/settings-scripts
+SETTINGSCRIPTS=/opt/ownsec/0.Initial/settings-scripts/startstop/0.Services
 BINDIR=/usr/local/bin
 XFCEMNUFLS=.local/share/applications
 
@@ -13,9 +13,9 @@ USR ENV PT1
 ${normal}"
 
 
-## SERVICE symlinks
+## services symlinks
 echo "${bold}
-... Symlink Services ....         
+... Symlink services ....         
 ${normal}"
 
 sudo updatedb
@@ -33,148 +33,161 @@ sudo rm -f /php7.0-fpm_*
 sudo rm -f /dnsmasq_*
 sudo rm -f /conky_*
 
-cd /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services
+echo "# Symlinks for mixed start stop update scripts in 0.Services menu"
+cd $SETTINGSCRIPTS
 for SHfiles in $(ls *.sh)
 do 
-sudo rm -f /usr/local/bin/$SHfiles
-sudo ln -s /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/$SHfiles /usr/local/bin/$SHfiles 
+sudo rm -f $BINDIR/$SHfiles
+sudo ln -s $SETTINGSCRIPTS/$SHfiles $BINDIR/$SHfiles 
 done
 
-cd /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/0.LAMP
+echo "# Symlinks for the LAMP stack services Scripts"
+cd $SETTINGSCRIPTS/0.LAMP
 for SHfiles in $(ls *.sh)
 do
-sudo rm -f /usr/local/bin/$SHfiles
-sudo ln -s /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/0.LAMP/$SHfiles /usr/local/bin/$SHfiles 
+sudo rm -f $BINDIR/$SHfiles
+sudo ln -s $SETTINGSCRIPTS/0.LAMP/$SHfiles $BINDIR/$SHfiles 
 done
 
-cd /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/0.LAPP
+echo "# Symlinks for the LAPP stack services Scripts"
+cd $SETTINGSCRIPTS/0.LAPP
 for SHfiles in $(ls *.sh)
 do
-sudo rm -f /usr/local/bin/$SHfiles
-sudo ln -s /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/0.LAPP/$SHfiles /usr/local/bin/$SHfiles 
+sudo rm -f $BINDIR/$SHfiles
+sudo ln -s $SETTINGSCRIPTS/0.LAPP/$SHfiles $BINDIR/$SHfiles 
 done
 
-cd /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/1.APACHE2
+echo "# Symlinks for the apache2 services Scripts"
+cd $SETTINGSCRIPTS/1.APACHE2
 for SHfiles in $(ls *.sh)
 do
-sudo rm -f /usr/local/bin/$SHfiles
-sudo ln -s /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/1.APACHE2/$SHfiles /usr/local/bin/$SHfiles
+sudo rm -f $BINDIR/$SHfiles
+sudo ln -s $SETTINGSCRIPTS/1.APACHE2/$SHfiles $BINDIR/$SHfiles
 done
 
-echo "test"
 
-cd /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/1.LIGHTTPD
+echo "# Symlinks for the lighttpd services Scripts"
+cd $SETTINGSCRIPTS/1.LIGHTTPD
 for SHfiles in $(ls *.sh)
 do
-sudo rm -f /usr/local/bin/$SHfiles
-sudo ln -s /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/1.LIGHTTPD/$SHfiles /usr/local/bin/$SHfiles
+sudo rm -f $BINDIR/$SHfiles
+sudo ln -s $SETTINGSCRIPTS/1.LIGHTTPD/$SHfiles $BINDIR/$SHfiles
 done
 
-cd /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/1.NGINX
+echo "# Symlinks for the nginx services Scripts"
+cd $SETTINGSCRIPTS/1.NGINX
 for SHfiles in $(ls *.sh)
 do
-sudo rm -f /usr/local/bin/$SHfiles
-sudo ln -s /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/1.NGINX/$SHfiles /usr/local/bin/$SHfiles
+sudo rm -f $BINDIR/$SHfiles
+sudo ln -s $SETTINGSCRIPTS/1.NGINX/$SHfiles $BINDIR/$SHfiles
 done
 
-cd /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/2.MYSQL
+echo "# Symlinks for the mysql services Scripts"
+cd $SETTINGSCRIPTS/2.MYSQL
 for SHfiles in $(ls *.sh)
 do
-sudo rm -f /usr/local/bin/$SHfiles
-sudo ln -s /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/2.MYSQL/$SHfiles /usr/local/bin/$SHfiles
+sudo rm -f $BINDIR/$SHfiles
+sudo ln -s $SETTINGSCRIPTS/2.MYSQL/$SHfiles $BINDIR/$SHfiles
 done
 
-cd /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/2.PSQL
+echo "# Symlinks for the psql services Scripts"
+cd $SETTINGSCRIPTS/2.PSQL
 for SHfiles in $(ls *.sh)
 do
-sudo rm -f /usr/local/bin/$SHfiles
-sudo ln -s /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/2.PSQL/$SHfiles /usr/local/bin/$SHfiles
+sudo rm -f $BINDIR/$SHfiles
+sudo ln -s $SETTINGSCRIPTS/2.PSQL/$SHfiles $BINDIR/$SHfiles
 done
 
-echo "test2"
-cd /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/2.REDIS
+echo "# Symlinks for the redis services Scripts"
+cd $SETTINGSCRIPTS/2.REDIS
 for SHfiles in $(ls *.sh)
 do
-sudo rm -f /usr/local/bin/$SHfiles
-sudo ln -s /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/2.REDIS/$SHfiles /usr/local/bin/$SHfiles
+sudo rm -f $BINDIR/$SHfiles
+sudo ln -s $SETTINGSCRIPTS/2.REDIS/$SHfiles $BINDIR/$SHfiles
 done
 
-cd /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/3.PHP
+echo "# Symlinks for the php services Scripts"
+cd $SETTINGSCRIPTS/3.PHP
 for SHfiles in $(ls *.sh)
 do
-sudo rm -f /usr/local/bin/$SHfiles
-sudo ln -s /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/3.PHP/$SHfiles /usr/local/bin/$SHfiles
+sudo rm -f $BINDIR/$SHfiles
+sudo ln -s $SETTINGSCRIPTS/3.PHP/$SHfiles $BINDIR/$SHfiles
 done
 
-cd /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/3.DNS
+echo "# Symlinks for the dns services Scripts"
+cd $SETTINGSCRIPTS/3.DNS
 for SHfiles in $(ls *.sh)
 do
-sudo rm -f /usr/local/bin/$SHfiles
-sudo ln -s /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/3.DNS/$SHfiles /usr/local/bin/$SHfiles
+sudo rm -f $BINDIR/$SHfiles
+sudo ln -s $SETTINGSCRIPTS/3.DNS/$SHfiles $BINDIR/$SHfiles
 done
 
-cd /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/4.TOR
+echo "# Symlinks for the tor services Scripts"
+cd $SETTINGSCRIPTS/4.TOR
 for SHfiles in $(ls *.sh)
 do
-sudo rm -f /usr/local/bin/$SHfiles
-sudo ln -s /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/4.TOR/$SHfiles /usr/local/bin/$SHfiles
+sudo rm -f $BINDIR/$SHfiles
+sudo ln -s $SETTINGSCRIPTS/4.TOR/$SHfiles $BINDIR/$SHfiles
 done
 
-echo "test3"
-cd /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/5.Conky
+echo "# Symlinks for the conky services Scripts"
+cd $SETTINGSCRIPTS/5.Conky
 for SHfiles in $(ls *.sh)
 do
-sudo rm -f /usr/local/bin/$SHfiles
-sudo ln -s /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/5.Conky/$SHfiles /usr/local/bin/$SHfiles
+sudo rm -f $BINDIR/$SHfiles
+sudo ln -s $SETTINGSCRIPTS/5.Conky/$SHfiles $BINDIR/$SHfiles
 done
-echo "test3s"
-cd /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/6.Docker
+
+echo "# Symlinks for the docker services Scripts"
+cd $SETTINGSCRIPTS/6.Docker
 for SHfiles in $(ls *.sh)
 do
-sudo rm -f /usr/local/bin/$SHfiles
-sudo ln -s /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/6.Docker/$SHfiles /usr/local/bin/$SHfiles
+sudo rm -f $BINDIR/$SHfiles
+sudo ln -s $SETTINGSCRIPTS/6.Docker/$SHfiles $BINDIR/$SHfiles
 done
 
-echo "test4"
-cd /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/7.Print-Scan
+echo "# Symlinks for the print & scan services Scripts"
+cd $SETTINGSCRIPTS/7.Print-Scan
 for SHfiles in $(ls *.sh)
 do
-sudo rm -f /usr/local/bin/$SHfiles
-sudo ln -s /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/7.Print-Scan/$SHfiles /usr/local/bin/$SHfiles
+sudo rm -f $BINDIR/$SHfiles
+sudo ln -s $SETTINGSCRIPTS/7.Print-Scan/$SHfiles $BINDIR/$SHfiles
 done
 
-cd /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/8.Bluetooth
+echo "# Symlinks for the bluetooth services Scripts"
+cd $SETTINGSCRIPTS/8.Bluetooth
 for SHfiles in $(ls *.sh)
 do
-sudo rm -f /usr/local/bin/$SHfiles
-sudo ln -s /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/8.Bluetooth/$SHfiles /usr/local/bin/$SHfiles
+sudo rm -f $BINDIR/$SHfiles
+sudo ln -s $SETTINGSCRIPTS/8.Bluetooth/$SHfiles $BINDIR/$SHfiles
 done
 
-cd /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/9.GPS
+echo "# Symlinks for the gps services Scripts"
+cd $SETTINGSCRIPTS/9.GPS
 for SHfiles in $(ls *.sh)
 do
-sudo rm -f /usr/local/bin/$SHfiles
-sudo ln -s /opt/ownsec/0.Initial/settings-scripts/startstop/0.Services/9.GPS/$SHfiles /usr/local/bin/$SHfiles
+sudo rm -f $BINDIR/$SHfiles
+sudo ln -s $SETTINGSCRIPTS/9.GPS/$SHfiles $BINDIR/$SHfiles
 done
 
 
 
-# script to copy and reload xfce configs from
-sudo rm -f /usr/local/bin/SCRIPT_copy-XFCE-conf
-sudo ln -s /opt/ownsec/0.Initial/settings-scripts/this-repo/SCRIPT_copy-XFCE-conf.sh /usr/local/bin/SCRIPT_copy-XFCE-conf
+echo "# Symlink script to copy and reload xfce configs"
+sudo rm -f $BINDIR/SCRIPT_copy-XFCE-conf
+sudo ln -s /opt/ownsec/0.Initial/settings-scripts/this-repo/SCRIPT_copy-XFCE-conf.sh $BINDIR/SCRIPT_copy-XFCE-conf
 
-# UPDATE script symlinks
-sudo rm -f /usr/local/bin/updateallthegits.sh
-sudo rm -f /usr/local/bin/upgrade-os.sh
+echo "# Symlinks for update-scripts"
+sudo rm -f $BINDIR/updateallthegits.sh
+sudo rm -f $BINDIR/upgrade-os.sh
 cd /opt/ownsec/00.Maintain
-sudo ln -s /opt/ownsec/00.Maintain/updateallthegits.sh /usr/local/bin/updateallthegits.sh
+sudo ln -s /opt/ownsec/00.Maintain/updateallthegits.sh $BINDIR/updateallthegits.sh
 cd /opt/ownsec/00.Maintain
-sudo ln -s /opt/ownsec/00.Maintain/upgrade-os.sh /usr/local/bin/upgrade-os.sh
+sudo ln -s /opt/ownsec/00.Maintain/upgrade-os.sh $BINDIR/upgrade-os.sh
 
 
-# XFCE menu links
+echo "# XFCE menu links"
 echo "${bold}
-... Create XFCE Menu Entries for Services ....         
+... Create XFCE Menu Entries for services ....         
 ${normal}"
 
 mkdir -p /home/$USER/$XFCEMNUFLS/0.Services
@@ -182,5 +195,6 @@ cp -R /opt/ownsec/0.Initial/usrlcl/$XFCEMNUFLS/0.Services /home/$USER/$XFCEMNUFL
 mkdir -p /home/$USER/$XFCEMNUFLS/00.Scripts
 cp -R /opt/ownsec/0.Initial/usrlcl/$XFCEMNUFLS/00.Scripts /home/$USER/$XFCEMNUFLS
 
+echo "# Finish Symlink Setup :)"
 sudo updatedb
 sudo ldconfig
