e2f5433165a37e3e9c4fc3b73deeb57dee078bbe
alphaaurigae <88888mb@gmail.com>
2018-01-19 04:40:06 +0100

    create 0b.Scripts folder in main dir

---
+++ 0b.Scripts/push-update-submodule.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Update submodule && workdir repo
+# Make changes on the submodule located in another git repository - push changes to remote  submodule && push submodule update to main repo.
+
+GITREPOROOT=/opt/ownsec
+SUBMODULE=/opt/ownsec/ITSEC-Install-Scripts-ORIG
+SUBMODULENAME=ITSEC-Install-Scripts-ORIG
+SUBMODULEBRANCH=ownsec-16.04_XFCE
+
+cd $SUBMODULE
+
+git add .
+git commit -a -m "commit in submodule"
+git push
+cd $GITREPOROOT
+git add $SUBMODULENAME
+git commit -m "Updated submodule"
